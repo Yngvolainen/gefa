c195ce79f413df24a58a1a133d3f5a1205fc9902
Yngvolainen <yngve.josevold@gmail.com>
2022-05-31 14:24:09 +0200

    remove unused files

--- src/sanity.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client';
-
-// export default sanityClient({
-//     projectId: '32td7jzv',
-//     dataset: 'production',
-//     apiVersion: '2022-03-30', //dagen prosjektet ble først opprettet/sist oppdatert
-//     useCdn: false //false for localhost, true for netlify
-// });
